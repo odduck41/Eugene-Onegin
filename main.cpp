537dab6af660503e33ce5de3f622d886a00a7f5f
odduck41 <gd.cats.creators@gmail.com>
2023-10-18 12:27:15 +0300

    Onegin V3.0

--- main.cpp
+++ main.cpp
@@ -11,14 +11,17 @@ int main() {
     a.print(a.get_rhythm(b[0]));
     a.print(b[1]);
     a.print(a.get_rhythm(b[1]));
+    std::cout << std::endl;
     a.print(b[2]);
     a.print(b[3]);
     a.print(a.get_rhythm(b[2]));
     a.print(a.get_rhythm(b[3]));
+    std::cout << std::endl;
     a.print(b[4]);
     a.print(b[5]);
     a.print(a.get_rhythm(b[5]));
     a.print(a.get_rhythm(b[4]));
+    std::cout << std::endl;
     a.print(b[6]);
     a.print(a.get_rhythm(b[6]));
 
